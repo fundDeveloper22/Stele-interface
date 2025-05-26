b7786151ea8d4a99f0ec131baf156ce37f1c14ab
ilotod <openfund365@gmail.com>
2025-05-26 10:43:05 +0900

    Update constants.ts
    created new subgraph
    
    stele_base -> stele-base-2

--- lib/constants.ts
+++ lib/constants.ts
@@ -18,9 +18,9 @@ export const USDC_TOKEN_ADDRESS = "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913"; 
 export const GOVERNANCE_CONTRACT_ADDRESS = "0x8f08aDA72cCEE0E1FaB09f25602b20624ED07599"; // Base Mainnet Governance
 
 // Token decimals
-export const USDC_DECIMALS = 6; 
+export const USDC_DECIMALS = 6;
 
 //subgraph
-//export const url = 'https://api.studio.thegraph.com/query/110372/stele_base/version/latest'
-export const url = 'https://gateway.thegraph.com/api/subgraphs/id/7j7CYjgLEhQZoAB2NR1EHTreZZT8z57tpYR2C51C5Sis'
+export const url = 'https://api.studio.thegraph.com/query/110372/stele-base-2/version/latest'
+//export const url = 'https://gateway.thegraph.com/api/subgraphs/id/7j7CYjgLEhQZoAB2NR1EHTreZZT8z57tpYR2C51C5Sis'
 export const headers = { Authorization: `Bearer ${process.env.NEXT_PUBLIC_THE_GRAPH_API_KEY}` }
