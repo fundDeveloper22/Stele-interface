6a65a7e7a275e7c71eb9effca691075f54e35ff3
ilotod <openfund365@gmail.com>
2025-06-05 06:14:11 +0900

    update to new deployed contract

--- lib/constants.ts
+++ lib/constants.ts
@@ -14,17 +14,17 @@ export const BASE_CHAIN_CONFIG = {
 export const BASE_BLOCK_TIME_MS = 2; // 2 seconds per block
 
 // Contract Addresses
-export const STELE_CONTRACT_ADDRESS = "0x7c23A7Ab6C52dfD529703df81B51d15cBF80376b";
-export const STELE_TOKEN_ADDRESS = "0x4b7940448EF781252A7F09c7A0998428432E970B"; // Stele Token for voting
+export const STELE_CONTRACT_ADDRESS = "0x30116F4a15146534a90B56c3Fbe26155ABEec1b5";
+export const STELE_TOKEN_ADDRESS = "0x6626806335C4BA670A7d25e70D21C1c007031b9F"; // Stele Token for voting
 export const USDC_TOKEN_ADDRESS = "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913"; // Base Mainnet USDC
-export const GOVERNANCE_CONTRACT_ADDRESS = "0xD1b825b0Cbe0059AAE6e9FC774d7Fc797347Ec67"; // Base Mainnet Governance
+export const GOVERNANCE_CONTRACT_ADDRESS = "0xcdb05851bB66A42DC451f3565a0397456B736acA"; // Base Mainnet Governance
 
 // Token decimals
 export const USDC_DECIMALS = 6;
 export const STELE_DECIMALS = 18;
 
 //subgraph
-export const SUBGRAPH_URL = 'https://api.studio.thegraph.com/query/111352/stele-base-5/version/latest'
+export const SUBGRAPH_URL = 'https://api.studio.thegraph.com/query/111352/stele-base-6/version/latest'
 export const headers = { Authorization: `Bearer ${process.env.NEXT_PUBLIC_THE_GRAPH_API_KEY}` }
 export const BYTE_ZERO = "0x00000000"
 
